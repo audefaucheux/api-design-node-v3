5c71fb3f9ff4a658dfeff8cf9be07f8e584f1821
Aude Faucheux <aude53@gmail.com>
2020-06-21 10:20:48 +0100

    add auth

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -16,10 +16,58 @@ export const verifyToken = token =>
     })
   })
 
-export const signup = async (req, res) => {}
+export const signup = async (req, res) => {
+  if (!req.body.email || !req.body.password) {
+    return res.status(400).send({ message: 'email and password required' })
+  }
 
-export const signin = async (req, res) => {}
+  try {
+    const user = await User.create(req.body)
+    const token = await newToken(user)
+    return res.status(201).send({ token })
+  } catch (error) {
+    return res.status(400).send({ message: error })
+  }
+}
+
+export const signin = async (req, res) => {
+  if (!req.body.email || !req.body.password) {
+    return res.status(400).send({ message: 'email and password required' })
+  }
+  const user = await User.findOne({ email: req.body.email }).exec()
+
+  if (!user) {
+    return res.status(401).send({ message: 'email does not exist' })
+  }
+
+  try {
+    const passwordMatch = await user.checkPassword(req.body.password)
+    if (!passwordMatch) {
+      return res.status(401).send({ message: 'password does not match' })
+    }
+    const token = await newToken(user)
+    return res.status(201).send({ token })
+  } catch (error) {
+    return res.status(401).send({ message: `${error}` })
+  }
+}
 
 export const protect = async (req, res, next) => {
-  next()
+  if (!req.headers.authorization) {
+    return res.status(401).end()
+  }
+
+  let token = req.headers.authorization.split('Bearer ')[1]
+
+  try {
+    const payload = await verifyToken(token)
+    const user = await User.findById(payload.id)
+      .select('-password')
+      .lean()
+      .exec()
+    req.user = user
+    next()
+  } catch (error) {
+    return res.status(401).end()
+  }
 }
